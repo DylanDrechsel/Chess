44da04676971448af862ed5f4e02436f75cc7277
Dylan Drechsel <drechseldylan@gmail.com>
2024-08-13 22:16:29 -0400

    Completed board rendering and pieces on the board

--- scripts/chessboard.js
+++ scripts/chessboard.js
@@ -24,7 +24,42 @@ class ChessBoard {
         // Loop through each row of the board
         for (let row = 0; row < 8; row++) {
             // Loop through each column of the board
+            for (let col = 0; col < 8; col++) {
+                // Create a new div element for the square
+                const square = document.createElement('div')
+                const piece = this.board[row][col]
+                console.log(piece)
+
+                // setting square color based on remainder 
+                square.className = `square ${((row + col) % 2 ===0) ? 'white' : 'black'}`
+                square.dataset.row = row
+                square.dataset.col = col
+
+                if (piece) {
+                    square.innerHTML = this.getPieceSymbol(piece)
+                }
+                
+                boardElement.appendChild(square);
+            }
         }
+    }
+
+    getPieceSymbol(piece) {
+        const symbols = {
+            'p': '♟',
+            'r': '♜',
+            'n': '♞',
+            'b': '♝',
+            'q': '♛',
+            'k': '♚',
+            'P': '♙',
+            'R': '♖',
+            'N': '♘',
+            'B': '♗',
+            'Q': '♕',
+            'K': '♔'
+        }
+        return symbols[piece]
     }
 }
 
