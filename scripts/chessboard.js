41df4368192bcabfa1c09f32c5a326cecad8b4b2
Dylan Drechsel <drechseldylan@gmail.com>
2024-08-14 12:15:11 -0400

    Completed selec and move piece functionality

--- scripts/chessboard.js
+++ scripts/chessboard.js
@@ -28,7 +28,6 @@ class ChessBoard {
                 // Create a new div element for the square
                 const square = document.createElement('div')
                 const piece = this.board[row][col]
-                console.log(piece)
 
                 // setting square color based on remainder 
                 square.className = `square ${((row + col) % 2 ===0) ? 'white' : 'black'}`
@@ -44,7 +43,58 @@ class ChessBoard {
         }
     }
 
-    getPieceSymbol(piece) {
+    addEventListeners = () => {
+        const squares = document.querySelectorAll('.square')
+        squares.forEach(square => {
+            // Bind 'this' to the current class instance for the event handler
+            square.addEventListener('click', this.handleSquareClick.bind(this))
+        })
+    }
+
+    handleSquareClick = (event) => {
+        // Gets the Row and Column indices from the clicked sqaures data attributes
+        // That were set in the renderBoard function
+        const row = parseInt(event.target.dataset.row) // Gets the row index
+        const col = parseInt(event.target.dataset.col) // Gets the column index
+
+        // Get the piece that is located at the clicked square
+        const piece = this.board[row][col]
+
+        // Check if the piece is currently selected
+        this.selectedPiece ? this.movePiece(row, col) : null
+        piece ? this.selectPiece(row,col) : null
+    }
+
+    // Function to select a piece on the board
+    selectPiece = (row, col) => {
+        // Store the selected piece's position in an object
+        this.selectedPiece = { row, col }
+
+        console.log(`Selected piece at ${row}, ${col}`);
+    }
+
+    // Function to move a piece from its selected position to a new position
+    movePiece = (row, col) => {
+        // Deconstruct the selected pieces starting position
+        const { row: startRow, col: startCol} = this.selectedPiece
+
+        // Move the selected piece to the new position on the baord
+        // Place the piece at the destination (row, col)
+        this.board[row][col] = this.board[startRow][startCol]
+
+        // Clear the starting position to indicate the piece has moved
+        this.board[startRow][startCol] = ''
+
+        // Reset the selectedPiece to null, indicating no piece is currently selected
+        this.selectPiece = null
+        
+        // Re-render the chessboard to update the position of the pieces
+        this.renderBoard()
+
+        console.log(`Moved piece to ${row} ${col}`)
+    }
+
+    getPieceSymbol = (piece) => {
         const symbols = {
             'p': '♟',
             'r': '♜',
@@ -65,3 +115,4 @@ class ChessBoard {
 
 const chessBoard = new ChessBoard()
 chessBoard.renderBoard()
+chessBoard.addEventListeners()
